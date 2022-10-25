3ada3cb65746bc96a6212984f0331ce5d6e650ac
konishi333 <jobhuntingkonishi3@gmail.com>
2022-10-25 18:27:41 +0900

    ActiveHash導入
    商品登録ページのプルダウンの項目が表示されるまで対応完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,15 @@
 class ItemsController < ApplicationController
   def index
+    # @categories = Category.order("created_at DESC")
+  end
+
+  def new
+    # @category = Category.new
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :status_id, :payment_method_id, :prefecture_id, :shipping_day_id, :image).merge(user_id: current_user.id)
+    # params.require(:item).permit(:name, :description, :category_id, :status_id, :payment_method_id, :prefecture_id, :shipping_day_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,26 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :payment_method
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  validates :name, presence: true
+  validates :discription, presence: true
+
+  #プルダウンの選択が「---」の時は保存できないように設定してある
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :payment_method_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  with_options presence: true, format: { with: VALID_NAME_REGEX, message: 'is ???',allow_blank: true } do
+    validates :seelling_price
+  end
+
 end
