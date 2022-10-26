9e4297b23f9b9ab3bf8e15e3f093ee70472106ac
konishi333 <jobhuntingkonishi3@gmail.com>
2022-10-26 23:16:20 +0900

    詳細情報を表示するコード実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    
+    # binding.pry
+    
+  end
+
   private
 
   def item_params
