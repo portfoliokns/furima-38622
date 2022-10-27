3d44d89cfe381edc81ebd7c93f4f06fba70943a2
konishi333 <jobhuntingkonishi3@gmail.com>
2022-10-27 23:16:39 +0900

    リファクタリング
    set_itemでインスタンス変数を先に設定し、move_to_indexでそのインスタンス変数が使えるように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit]
   before_action :set_item, only: [:show, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -43,8 +43,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    redirect_to root_path unless item.user_id == current_user.id
+    redirect_to root_path unless @item.user_id == current_user.id
   end
 
   def set_item
