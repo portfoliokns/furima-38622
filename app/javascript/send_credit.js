7bb09cacbe5663a5ec85a8ec89e68e01581a1ba7
konishi333 <jobhuntingkonishi3@gmail.com>
2022-10-30 17:39:07 +0900

    クレジットカード情報処理（クライアントサイド）
    クレジットカード情報をPay.jpへ送信し、トークンを発行してもらい、それをサーバーサイドへ送信するまでを実装

--- app/javascript/send_credit.js
+++ app/javascript/send_credit.js
@@ -1,8 +1,34 @@
 function send_credit (){
+  Payjp.setPublicKey("pk_test_cbfe17c56a7aa9716f200004");
   const submit = document.getElementById('button');
   submit.addEventListener("click",(e) => {
     e.preventDefault();
-    console.log('発火')
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+    const card = {
+      number: formData.get("order_delivery[card_number]"),
+      cvc: formData.get("order_delivery[card_cvc]"),
+      exp_month: formData.get("order_delivery[card_exp_month]"),
+      exp_year: `20${formData.get("order_delivery[card_exp_year]")}`,
+    };
+    // console.log(card)
+    // debugger
+    // console.log('発火')
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      };
+      document.getElementById("card_number").removeAttribute("name");
+      document.getElementById("card_cvc").removeAttribute("name");
+      document.getElementById("card_exp_month").removeAttribute("name");
+      document.getElementById("card_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
   });
 
 };
