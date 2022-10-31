8855948bc1a9901e8e2cc5ed214040ee6cbbf035
konishi333 <jobhuntingkonishi3@gmail.com>
2022-10-31 14:14:22 +0900

    一つ前のpushが正しく反映されなかったため、復旧

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if @item.user_id == current_user.id
+    redirect_to root_path if @item.user_id == current_user.id || @item.order.present?
   end
 
   def pay_item
